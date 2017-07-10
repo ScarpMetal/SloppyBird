f03c824e8e54df933f6389902332df54ddf4aea2
iD Staff <idstaff@stratton-thirteen.mit.edu>
2017-07-10 11:38:05 -0400

    Initial Commit. Spawning pipes works.

--- SloppyBird/GameScene.swift
+++ SloppyBird/GameScene.swift
@@ -9,79 +9,71 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bird : SKSpriteNode = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 40, height: 40))
+    let GAP_SIZE : CGFloat = 200
+    let pipeWidth : CGFloat = 80
+    let pipeHeight : CGFloat = 400
     
     override func didMove(to view: SKView) {
+        bird.position = CGPoint(x: -size.width/4, y: 200)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        bird.physicsBody = SKPhysicsBody(rectangleOf: bird.size)
+        bird.physicsBody?.mass = 1
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        addChild(bird)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        physicsWorld.gravity = CGVector(dx: 0, dy: -12.81)
+        
+        run(SKAction.repeatForever(SKAction.sequence([
+            SKAction.run(spawnPipes),
+            SKAction.wait(forDuration: 2.0)
+            ]))
+        )
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func spawnPipes(){
+        
+        let gapYPosition : CGFloat = CGFloat(arc4random_uniform(UInt32(size.height/4))) - size.height/8
+        let topPipe = SKSpriteNode(color: UIColor.green, size: CGSize(width: pipeWidth, height: pipeHeight))
+        let bottomPipe = SKSpriteNode(color: UIColor.green, size: CGSize(width: pipeWidth, height: pipeHeight))
+        
+        topPipe.position.y = gapYPosition + GAP_SIZE/2 + (pipeHeight / 2)
+        bottomPipe.position.y = gapYPosition - GAP_SIZE/2 - (pipeHeight / 2)
+        
+        topPipe.position.x = size.width/2 + pipeWidth/2
+        bottomPipe.position.x = size.width/2 + pipeWidth/2
+        
+        addChild(topPipe)
+        addChild(bottomPipe)
+        
+        let topPipeAction = SKAction.move(to: CGPoint(x: -size.width/2 - topPipe.size.width/2, y: topPipe.position.y), duration: 5.0)
+        let bottomPipeAction = SKAction.move(to: CGPoint(x: -size.width/2 - bottomPipe.size.width/2, y: bottomPipe.position.y), duration: 5.0)
+        
+        topPipe.run(topPipeAction)
+        bottomPipe.run(bottomPipeAction)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        bird.run(SKAction.applyImpulse(CGVector(dx: 0, dy: 750), duration: 0.05))
+
     }
+
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
